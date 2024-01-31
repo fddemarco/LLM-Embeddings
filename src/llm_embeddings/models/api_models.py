2ce01fa4febe4b1b254f26cb09a782f740445254
FrancoD <franco.demarco3400@gmail.com>
2024-01-31 11:25:59 -0300

    enhancement: rename methods

--- src/llm_embeddings/models/api_models.py
+++ src/llm_embeddings/models/api_models.py
@@ -5,19 +5,19 @@ import voyageai
 
 class Model:
     def embed(self, sentences):
-        self.assert_sentences(sentences)
-        sentences = self.truncate_sentences(sentences)
+        self.assert_are_valid(sentences)
+        sentences = self.truncate(sentences)
         return self.safe_embed(sentences)
 
-    def assert_sentences(self, sentences):
+    def assert_are_valid(self, sentences):
         self.assert_not_empty(sentences)
-        self.assert_batch_size(sentences)
+        self.assert_allowed_batch_size(sentences)
 
     def assert_not_empty(self, sentences):
         if not sentences:
             raise ValueError("Input cannot be empty list")
 
-    def assert_batch_size(self, sentences):
+    def assert_allowed_batch_size(self, sentences):
         if len(sentences) > 128:
             raise ValueError(
                 f"The batch size limit is 128. Your batch size is {len(sentences)}"
@@ -26,7 +26,7 @@ class Model:
     def safe_embed(self, sentences):
         raise NotImplemented("Should be implemented in a subclass")
 
-    def truncate_sentences(self, sentences):
+    def truncate(self, sentences):
         raise NotImplemented("Should be implemented in a subclass")
 
 
@@ -43,7 +43,7 @@ class VoyageAiModel(Model):
             sentences, model="voyage-lite-01", input_type="document", truncation=True
         )
 
-    def truncate_sentences(self, sentences):
+    def truncate(self, sentences):
         return sentences
 
 
@@ -66,7 +66,7 @@ class Ada2Model(Model):
             input=sentences, model="text-embedding-ada-002"
         )
 
-    def truncate_sentences(self, sentences):
+    def truncate(self, sentences):
         encoder = tiktoken.get_encoding(self.embedding_encoding)
         return [
             encoder.encode(sentence)[: self.max_input_tokens] for sentence in sentences
